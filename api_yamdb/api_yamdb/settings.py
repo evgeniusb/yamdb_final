ee0d8e531d4fe6d69f73a5f889cfae7dba08b308
Evgeniusb <dos18i2-github@yahoo.com>
2023-01-27 13:59:59 +0300

    В settings.py корректно добавлено значение ENGINE.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv(
             'DB_ENGINE',
-            default='django.db.backends.postgresql')
+            default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
